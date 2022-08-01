8630df8208844668adc531e53cc05ca09adba985
MGonza20 <64711979+MGonza20@users.noreply.github.com>
2022-08-01 15:03:27 -0600

    Changing size to have a better perspective.

--- exercise3.py
+++ exercise3.py
@@ -3,14 +3,14 @@
 from library import Renderer, color, V2, V3
 import random
 
-width = 540
-height = 500
+width = 1920
+height = 1080
 
 rend = Renderer(width, height)
 
 rend.glLoadModel("Mandalorian.obj", 
-                 translate= V3(width/2, height/3.5, 0), 
-                 scale= V3(10, 10, 10))
+                 translate= V3(width/2, height/4, 0), 
+                 scale= V3(24, 24, 24))
 
 rend.glFinish("Mandalorian.bmp")
 
